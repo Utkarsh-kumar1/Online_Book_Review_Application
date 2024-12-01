397fe569e3add91c764ba6ca4e3c805122614717
Utkarsh_kumar1 <kumarutkarsh210@gmail.com>
2024-12-01 13:08:56 +0530

    first commit

---
+++ controllers/book.js
@@ -0,0 +1,101 @@
+import book from "../models/book.js"
+
+export async function getAllBooks(req, res) {
+    try {
+        const books = await book.findAll();
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!!" });
+    }
+}
+
+export async function addBook(req, res) {
+    try {
+
+        console.log(req.body);
+        
+        const foundBook = await book.findOne({ where: req.body });
+        console.log(foundBook);
+        
+        if (foundBook) {
+            return res.json({ message: "Book Already Found!!" });
+        }
+
+        const newBook = await book.create(req.body);
+        res.json({ message: "Book Added Successfully!" });
+
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({ message: "Internal Server Error!!" });
+    }
+}
+
+export async function getBooksByISBN(req, res) {
+    try {
+        
+
+        const { ISBN } = req.query;
+        
+
+        if (!ISBN) {
+            return res.json({ message: "Please, provide a valid ISBN Code!" });
+        }
+
+        const foundBooks = await book.findAll({ where: { ISBN } });
+        if (foundBooks.length) {
+            return res.json({ message: "Books are Found!!", foundBooks });
+        }
+
+        res.json({ message: "No book found with this ISBN Code!" });
+    } catch (error) {
+
+        console.log(error);
+        
+        res.status(500).json({ message: "Internal Server Error!!" });
+
+    }
+}
+
+export async function getBooksByTitle(req, res) {
+    try {
+
+        const { title } = req.query;
+
+        if (!title) {
+            return res.json({ message: "Please, provide a valid title!" });
+        }
+
+        const foundBooks = await book.findAll({ where: { title } });
+        if (foundBooks.length) {
+            return res.json({ message: "Books are Found!!", foundBooks });
+        }
+
+        res.json({ message: "No book found with this title!" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!!" });
+    }
+}
+
+export async function getBooksByAuthor(req, res) {
+    try {
+
+        const { author } = req.query;
+
+        if (!author) {
+            return res.json({ message: "Please, provide a valid author name!" });
+        }
+
+        const foundBooks = await book.findAll({ where: { author } });
+        if (foundBooks.length) {
+            return res.json({ message: "Books are Found!!", foundBooks });
+        }
+
+        res.json({ message: "No book found with this author name!" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!!" });
+    }
+}
+
